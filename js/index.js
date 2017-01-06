bdab250f973abd5d8ea0d437780d5b352d0fe89f
luchtrix58CF <lalito5828@gmail.com>
2017-01-05 18:28:44 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -17,6 +17,61 @@
  * under the License.
  */
 var app = {
+//var appmain = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        alert("onDeviceReady");
+        console.log("onDeviceReady");
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"369410224872","ecb":"app.onNotificationGCM"});
+        // result contains any message sent from the plugin call
+        /*console.log('Received Device Ready Event');
+        //console.log('calling setup push');
+        app.setupPush();*/
+    },
+    successHandler: function(result) {
+        alert('Callback Success! Result = '+result);
+    },
+    errorHandler:function(error) {
+        alert(error);
+    },
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+                break; 
+            case 'message':
+                // this is the actual push notification. its format depends on the data model from the push server
+                alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break; 
+            case 'error':
+                alert('GCM error = '+e.msg);
+                break;
+            default:
+                alert('An unknown GCM event has occurred');
+                break;
+        }
+    }
+/*var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -110,5 +165,5 @@ var app = {
                 'Ok'                  // buttonName
             );
        });
-    }
+    }*/
 };
